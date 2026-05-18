8c78d82a28e16c96bb28dfbb7d6bd93c1c4e02a1
fanwwi <farangiz.anvarjanova553@gmail.com>
2026-05-19 01:24:28 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Letter from "./components/Letter";
 import cakeVideo from "./assets/cake2.webm";
+import fireworksVideo from "./assets/fireworks2.webm";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -17,17 +18,23 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Happy birthday, Daniil!🥳</h1>
-
-      <button onClick={() => setOpen(true)} className="openBtn">
-        Открой 💌
-      </button>
-
-      <video autoPlay muted loop playsInline className="cakeVideo">
-        <source src={cakeVideo} type="video/mp4" />
+      <video autoPlay muted loop playsInline className="bgVideo">
+        <source src={fireworksVideo} type="video/mp4" />
       </video>
 
-      {open && <Letter closing={closing} onClose={handleClose} />}
+      <div className="content">
+        <h1>Happy birthday, Daniil!🥳</h1>
+
+        <button onClick={() => setOpen(true)} className="openBtn">
+          Открой 💌
+        </button>
+
+        <video autoPlay muted loop playsInline className="cakeVideo">
+          <source src={cakeVideo} type="video/mp4" />
+        </video>
+
+        {open && <Letter closing={closing} onClose={handleClose} />}
+      </div>
     </div>
   );
 }
